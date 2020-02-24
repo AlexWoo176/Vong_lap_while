2c846f79a9748dd7e3f4f446908ed8b6ae71cd40
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-02-24 14:31:27 +0700

    Complete

---
+++ js/appBT1.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt("Enter a number: "));
+let total = 0;
+while (num != -1) {
+    num = parseInt(prompt("Enter a number: "));
+    alert(num);
+    total = total + num;
+}
+    alert(total);
